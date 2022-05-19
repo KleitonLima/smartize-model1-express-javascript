fa2adfdb0d053918f8f3cf3cb930aa300e9a464b
Kleiton Lima <kleiton.mini@gmail.com>
2022-05-19 19:42:31 -0300

    Criação da API

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import {produtos} from './databases'
+
+const port = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/produtos', (req, res) => {
+  res.send(produtos);
+});
+app.get('/produtos/:id', (req, res) => {
+  const idProduto = req.params.id;
+  const atualProduto = produtos.find((produtos) => produtos.id == idProduto);
+  res.send(atualProduto);
+});
+
+app.listen(port, () => {
+  console.log(`Rodando na porta http://localhost:${port}`);
+});
