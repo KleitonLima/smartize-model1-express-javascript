61b52b5021380104caeb74e5072e773ec7ff7877
Kleiton Lima <kleiton.mini@gmail.com>
2022-06-13 22:03:17 -0300

    fix: ports

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import 'dotenv/config';
 import app from './app';
 import conexaoDatabase from './databases';
 
+const port = process.env.PORT || 3001;
+
 app.listen(process.env.PORT, () => {
   conexaoDatabase();
+  if (!process.env.PORT) {
+    console.log(`Rodando na porta http://localhost:${port}`);
+  }
 });
